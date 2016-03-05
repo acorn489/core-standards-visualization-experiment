bedfe2ca62498a7dced57481fe337ba8c1857d23
prismacon <wwhhooaahh@gmail.com>
2016-03-05 16:17:36 +0100

    Execute "build" synchronously
    Livereload was ready before build, so browser didn't always open.
    
    Use an alternative livereload plugin which allows manual trigger of
    refresh.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,3 +1,5 @@
+/*globals process:false*/
+
 import "babel-polyfill";
 import gulp from "gulp";
 import del from "del";
@@ -7,10 +9,14 @@ import adaptDataForTemplate from "./src/adaptDataForTemplate";
 import fs from "fs";
 import mocha from "gulp-mocha";
 import babel from "babel-core/register";
-import livereload from "gulp-server-livereload";
+import livereload from "gulp-livereload";
 import http from "http";
 import ecstatic from "ecstatic";
+import runSequence from "run-sequence";
 import {loadFile} from "./src/helpers";
+import gutil from "gulp-util";
+
+let SERVER_ADDRESS = "0.0.0.0";
 
 gulp.task("clean", () => {
   return del(["build"]);
@@ -21,31 +27,40 @@ gulp.task("test", () => {
     .pipe(mocha({compilers: {js: babel}}));
 });
 
-gulp.task("build", ["clean"], () => {
+gulp.task("build", ["clean"], (done) => {
   extractXMLData(loadFile("../resource/math.xml"))
     .then(data => {
       let viewData = adaptDataForTemplate(data);
       fs.mkdirSync("build");
       fs.writeFileSync("build/data.json", JSON.stringify(data));
       fs.writeFileSync("build/index.html", generateHTML(viewData));
+      livereload.reload();
+      done();
     })
     .catch(e => console.log(e, e.stack));
 });
 
 gulp.task("watch", () => {
+  livereload.listen();
   gulp.watch(
     ["src/**/*.js", "src/**/*.handlebars", "resource/**/*.json", "test/**/*.js"],
     ["build"]
   );
 });
 
-gulp.task("default", ["build", "watch"], () => {
-  gulp.src("./build")
-    .pipe(livereload({livereload: true, open: true}));
+gulp.task("deploy", () => {
+  let port = process.env.PORT || 8080;
+  let server = http.createServer(
+    ecstatic({root: __dirname + "/build"})
+  ).listen(port, SERVER_ADDRESS, () => gutil.log(
+    gutil.colors.red(`Listening on http://${server.address().address}:${port}`)
+  ));
 });
 
-gulp.task("heroku", ["build"], () => {
-  http.createServer(
-    ecstatic({ root: __dirname + "/build" })
-  ).listen(process.env.PORT || 8080);
+gulp.task("default", () => {
+  runSequence("build", "watch", "deploy");
+});
+
+gulp.task("heroku", () => {
+  runSequence("build", "deploy");
 });
